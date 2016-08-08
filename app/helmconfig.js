b06a83beb93fb545b63b984fbb36fd1825e18732
Joshua Henson <joshuahenson@users.noreply.github.com>
2016-08-08 00:43:55 -0700

    Remove unused images

--- app/helmconfig.js
+++ app/helmconfig.js
@@ -10,23 +10,13 @@
  */
 
 // Import all your needed files first (webpack will grab the url)
-import chromecon from 'images/chrome-ninja192-precomposed.png';
-import applecon from 'images/apple-ninja152-precomposed.png';
-import mscon from 'images/ms-ninja144-precomposed.png';
 import favicon from 'images/favicon.png';
 
 const config = {
   link: [
-    // Add to homescreen for Chrome on Android
     { rel: 'icon', href: favicon },
-    { rel: 'icon', sizes: '192x192', href: chromecon },
-    // Add to homescreen for Safari on IOS
-    { rel: 'apple-touch-icon', sizes: '152x152', applecon },
-    { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto+Condensed', type: 'text/css' },
+    { rel: 'stylesheet', href: 'https://maxcdn.bootstrapcdn.com/bootswatch/3.3.6/slate/bootstrap.min.css' },
     { rel: 'stylesheet', href: '/assets/styles/main.css' }
-    // SEO: If your mobile URL is different from the desktop URL,
-    // add a canonical link to the desktop page https://developers.google.com/webmasters/smartphone-sites/feature-phones
-    // { 'rel': 'canonical', 'href': 'http://www.example.com/' }
   ],
   meta: [
     { charset: 'utf-8' },
@@ -43,10 +33,7 @@ const config = {
     // Add to homescreen for Safari on IOS
     { name: 'apple-mobile-web-app-capable', content: 'yes' },
     { name: 'apple-mobile-web-app-status-bar-style', content: 'black' },
-    { name: 'apple-mobile-web-app-title', content: 'React Webpack Node' },
-    // Tile icon for Win8 (144x144 + tile color)
-    { name: 'msapplication-TileImage', content: mscon },
-    { name: 'msapplication-TileColor', content: '#3372DF' }
+    { name: 'apple-mobile-web-app-title', content: 'React Webpack Node' }
   ]
 };
 
