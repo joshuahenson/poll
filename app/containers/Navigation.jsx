84a265991604b18951002241b30a2288f5454564
Joshua Henson <joshuahenson@users.noreply.github.com>
2016-08-12 08:15:34 -0700

    Bootstrapped Navigation

--- app/containers/Navigation.jsx
+++ app/containers/Navigation.jsx
@@ -1,28 +1,41 @@
 import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
 import { connect } from 'react-redux';
-import { logOut } from 'actions/users';
-
-import classNames from 'classnames/bind';
-import styles from 'css/components/navigation';
-
-const cx = classNames.bind(styles);
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import { logOut } from '../actions/users';
 
 const Navigation = ({ user, logOut }) => {
     return (
-      <nav className={cx('navigation')} role="navigation">
-        <Link to="/"
-          className={cx('item', 'logo')}
-          activeClassName={cx('active')}>Ninja Ocean</Link>
-          { user.authenticated ? (
-            <Link onClick={logOut}
-              className={cx('item')} to="/">Logout</Link>
-          ) : (
-            <Link className={cx('item')} to="/login">Log in</Link>
+      <Navbar fluid>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <Link to="/">Pollster</Link>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav>
+            <LinkContainer to="/about">
+              <NavItem>About</NavItem>
+            </LinkContainer>
+          </Nav>
+          <Nav pullRight>
+          { user.authenticated ? ([
+            <LinkContainer to="/dashboard">
+              <NavItem>Dashboard</NavItem>
+            </LinkContainer>,
+            <LinkContainer active={false} onClick={logOut} to="/">
+              <NavItem>Logout</NavItem>
+            </LinkContainer>
+          ]) : (
+            <LinkContainer to="/login">
+              <NavItem>Login</NavItem>
+            </LinkContainer>
           )}
-          <Link className={cx('item')} to="/dashboard">Dashboard</Link>
-          <Link to="/about" className={cx('item')} activeClassName={cx('active')}>About</Link>
-      </nav>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     );
 };
 
@@ -37,4 +50,4 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps, { logOut })(Navigation);
+export default connect(mapStateToProps, { logOut }, null, { pure: false })(Navigation);
