d88b359c95b495bfe4f2003a02d9ff8b4b147ae3
Joshua Henson <joshuahenson@users.noreply.github.com>
2016-08-15 15:19:28 -0700

    Initial Login

--- app/containers/LoginOrRegister.jsx
+++ app/containers/LoginOrRegister.jsx
@@ -1,103 +1,106 @@
-import React, { Component, PropTypes } from 'react';
-import ReactDOM from 'react-dom';
-import classNames from 'classnames/bind';
-import { connect } from 'react-redux';
-import { manualLogin, signUp, toggleLoginMode } from 'actions/users';
-import styles from 'css/components/login';
-import hourGlassSvg from 'images/hourglass.svg';
+import React, {Component, PropTypes} from 'react';
+import {reduxForm} from 'redux-form';
+import {manualLogin, signUp, toggleLoginMode} from '../actions/users';
 
-const cx = classNames.bind(styles);
+const validate = values => {
+  const errors = {};
+  if (!values.email) {
+    errors.email = 'Required';
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'Invalid email address';
+  }
+  if (!values.password) {
+    errors.password = 'Required';
+  }
+  return errors;
+};
 
 class LoginOrRegister extends Component {
-  /*
-   * This replaces getInitialState. Likewise getDefaultProps and propTypes are just
-   * properties on the constructor
-   * Read more here: https://facebook.github.io/react/blog/2015/01/27/react-v0.13.0-beta-1.html#es6-classes
-   */
   constructor(props) {
     super(props);
-    this.handleOnSubmit = this.handleOnSubmit.bind(this);
+    this.submit = this.submit.bind(this);
   }
-
-  handleOnSubmit(event) {
-    event.preventDefault();
-
-    const { manualLogin, signUp, user: { isLogin } } = this.props;
-    const email = ReactDOM.findDOMNode(this.refs.email).value;
-    const password = ReactDOM.findDOMNode(this.refs.password).value;
+  submit() {
+    const {manualLogin, signUp, user: {isLogin}, fields: { email, password } } = this.props;
+    const emailPass = {
+      email: email.value,
+      password: password.value
+    };
 
     if (isLogin) {
-      manualLogin({ email, password });
+      manualLogin(emailPass);
     } else {
-      signUp({ email, password });
+      signUp(emailPass);
     }
   }
-
+  loginButtonText() {
+    if (this.props.user.isLogin) {
+      return 'Login';
+    }
+    return 'Register';
+  }
   renderHeader() {
-    const { user: { isLogin } , toggleLoginMode } = this.props;
+    const {user: {isLogin}, toggleLoginMode} = this.props;
     if (isLogin) {
       return (
-        <div className={cx('header')}>
-          <h1 className={cx('heading')}>Login with Email</h1>
-          <div className={cx('alternative')}>
-            Not what you want?
-            <a className={cx('alternative-link')}
-              onClick={toggleLoginMode}> Register an Account</a>
+        <div className="text-center">
+          <h2>Login with Email</h2>
+          <div>
+            If you don't yet have an account. &nbsp;
+            <a onClick={toggleLoginMode}>
+              Register Here
+            </a>
           </div>
         </div>
       );
     }
 
     return (
-      <div className={cx('header')}>
-      <h1 className={cx('heading')}>Register with Email</h1>
-        <div className={cx('alternative')}>
-          Already have an account?
-          <a className={cx('alternative-link')}
-            onClick={toggleLoginMode}> Login</a>
+      <div className="text-center">
+        <h2>Register with Email</h2>
+        <div>
+          Already have an account? &nbsp;
+          <a onClick={toggleLoginMode}>
+            Login Here
+          </a>
+          <p>Add name option</p>
         </div>
       </div>
     );
   }
-
   render() {
-    const { isWaiting, message, isLogin } = this.props.user;
-
+    const { fields: { email, password }, resetForm, handleSubmit, submitting } = this.props;
     return (
-      <div className={cx('login', {
-        waiting: isWaiting
-      })}>
-        <div className={cx('container')}>
-          { this.renderHeader() }
-          <img className={cx('loading')} src={hourGlassSvg} />
-          <div className={cx('email-container')}>
-            <form onSubmit={this.handleOnSubmit}>
-              <input className={cx('input')}
-              type="email"
-              ref="email"
-              placeholder="email" />
-              <input className={cx('input')}
-              type="password"
-              ref="password"
-              placeholder="password" />
-              <div className={cx('hint')}>
-              <div>Hint</div>
-              <div>email: example@ninja.com password: ninja</div>
-              </div>
-              <p className={cx('message', {
-                'message-show': message && message.length > 0
-              })}>{message}</p>
-              <input className={cx('button')}
-                type="submit"
-                value={isLogin ? 'Login' : 'Register'} />
-            </form>
+      <div>
+        { this.renderHeader() }
+        <form className="form-horizontal" onSubmit={handleSubmit(this.submit)}>
+          <div className={'form-group' + (email.touched && email.error ? ' has-error' : '')}>
+            <label htmlFor="emailInput" className="col-sm-2 control-label">Email</label>
+            <div className={'col-sm-' + (email.touched && email.error ? '5' : '8')}>
+              <input id="emailInput" type="text" className="col-sm-8 form-control" placeholder="Email" {...email} />
+            </div>
+            {email.touched && email.error && <div className="col-sm-3 help-block">{email.error}</div>}
           </div>
-          <div className={cx('google-container')}>
-            <h1 className={cx('heading')}>Google Login Demo</h1>
-            <a className={cx('button')}
-          href="/auth/google">Login with Google</a>
+          <div className={'form-group' + (password.touched && password.error ? ' has-error' : '')}>
+            <label htmlFor="pwInput" className="col-sm-2 control-label">Password</label>
+            <div className={'col-sm-' + (password.touched && password.error ? '5' : '8')}>
+              <input id="pwInput" type="password" className="col-sm-8 form-control" placeholder="Password" {...password} />
+            </div>
+            {password.touched && password.error && <div className="col-sm-3 help-block">{password.error}</div>}
           </div>
-        </div>
+          <div className="text-center">
+            <button type="submit" className="btn btn-primary btn-lg" style={{ margin: 10 }} disabled={submitting}>
+              {submitting ? 'Submitting...' : this.loginButtonText()}
+            </button>
+            <button type="button" className="btn btn-default btn-lg" style={{ margin: 10 }} disabled={submitting} onClick={resetForm}>
+              Clear Values
+            </button>
+          </div>
+          <div className="text-center">
+            <h2>Google Login needs setup</h2>
+            <a className="btn btn-default btn-lg" href="/auth/google">Login with Google</a>
+          </div>
+        </form>
       </div>
     );
   }
@@ -113,13 +116,16 @@ LoginOrRegister.propTypes = {
 // Function passed in to `connect` to subscribe to Redux store updates.
 // Any time it updates, mapStateToProps is called.
 function mapStateToProps({user}) {
-  return {
-    user
-  };
+  return {user};
 }
 
-// Connects React component to the redux store
-// It does not modify the component class passed to it
-// Instead, it returns a new, connected component class, for you to use.
-export default connect(mapStateToProps, { manualLogin, signUp, toggleLoginMode })(LoginOrRegister);
+const formConfig = {
+  form: 'login',
+  fields: [
+    'email', 'password'
+  ],
+  validate
+};
 
+// reduxForm connects redux store/form to app
+export default reduxForm(formConfig, mapStateToProps, {manualLogin, signUp, toggleLoginMode})(LoginOrRegister);
