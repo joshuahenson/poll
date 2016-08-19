dd2d381b064fdae6edf3ecc1399f1ab00b0b3b87
Joshua Henson <joshuahenson@users.noreply.github.com>
2016-08-18 18:06:10 -0700

    Removed logger

--- app/store/configureStore.js
+++ app/store/configureStore.js
@@ -1,9 +1,9 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import { routerMiddleware } from 'react-router-redux';
 import thunk from 'redux-thunk';
-import rootReducer from 'reducers';
-import promiseMiddleware from 'middlewares/promiseMiddleware';
-import createLogger from 'redux-logger';
+import rootReducer from '../reducers';
+import promiseMiddleware from '../middlewares/promiseMiddleware';
+// import createLogger from 'redux-logger';
 
 /*
  * @param {Object} initial state to bootstrap our stores with for server-side rendering
@@ -17,7 +17,7 @@ export default function configureStore(initialState, history) {
   let store;
 
   if (__DEVCLIENT__) {
-    middleware.push(createLogger());
+    // middleware.push(createLogger());
     store = createStore(rootReducer, initialState, compose(
       applyMiddleware(...middleware),
       typeof window === 'object' && typeof window.devToolsExtension !== 'undefined' ? window.devToolsExtension() : f => f
@@ -29,7 +29,8 @@ export default function configureStore(initialState, history) {
   if (module.hot) {
     // Enable Webpack hot module replacement for reducers
     module.hot.accept('reducers', () => {
-      const nextReducer = require('reducers');
+      const nextReducer = require('../reducers');
+
       store.replaceReducer(nextReducer);
     });
   }
