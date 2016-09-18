91a87574250da7fe621659ea6e947d06a6511457
Joshua Henson <joshuahenson@users.noreply.github.com>
2016-09-17 20:07:20 -0700

    Created Spinner component

---
+++ app/components/Spinner.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// Found at http://tobiasahlin.com/spinkit/
+const Spinner = () => {
+  return (
+    <div className="spinner">
+      <div className="double-bounce1" />
+      <div className="double-bounce2" />
+    </div>
+  );
+};
+
+export default Spinner;
